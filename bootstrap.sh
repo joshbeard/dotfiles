e05a66c04d27656c1f4da824c940604824bf8486
Josh Beard <josh@signalboxes.net>
2015-10-22 21:12:11 -0600

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,6 @@ homesick clone joshbeard/dotfiles
 homesick link dotfiles
 
 ## Install vundle for vim
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim +PluginInstall +qall
