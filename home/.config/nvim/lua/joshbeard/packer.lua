417fbe7aaf065b7d970444571943606f576a9696
Josh Beard <josh@joshbeard.me>
2023-06-05 23:15:02 -0600

    nvim/packer: bootstrap

--- home/.config/nvim/lua/joshbeard/packer.lua
+++ home/.config/nvim/lua/joshbeard/packer.lua
@@ -1,7 +1,29 @@
-vim.cmd.packadd('packer.nvim')
+-- Neovim Packer Configuration for Plugins
+-- https://github.com/wbthomason/packer.nvim
+
+-- Bootstrap packer.nvim if not installed
+-- https://github.com/wbthomason/packer.nvim#bootstrapping
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
+
+    -- https://github.com/wbthomason/packer.nvim#bootstrapping
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+
     --  use 'rstacruz/vim-closer'
 
     use {
