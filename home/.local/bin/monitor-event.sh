42a8038d84970e4500d12456e23d929fd7167731
Josh Beard <josh@joshbeard.me>
2023-11-05 13:10:46 -0700

    monitor-event: notify icon, cleanups

--- home/.local/bin/monitor-event.sh
+++ home/.local/bin/monitor-event.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # joshbeard's monitor layout script for X11/Xorg
-# 
+# =============================================================================
 # Script to automatically change monitor layout based on connected monitors.
 # This script is called by the 'srandrd' daemon and uses 'xrandr' to change
 # the layout.
@@ -19,7 +19,12 @@
 # Perhaps there's something out there that already does this in a fairly
 # agnostic sort of way?
 
-# Define corresponding layout scripts as an associative array
+# ----------------------------------------------------------------------------
+# Configuration
+
+# Define corresponding layout scripts as an associative array.
+# The hash is the MD5 hash of the EDID of the connected monitors. Run this
+# script manually to get the hash for the current configuration.
 declare -A LAYOUTS=(
   ["223632c428784fecaaa3e2a6aaaf6d8e"]="No monitors:${HOME}/.screenlayout/laptop-only.sh"
   ["5564e8f6c8dacb3fca826f532ff7ef73"]="Workbench Single:${HOME}/.screenlayout/workbench-single.sh"
@@ -29,8 +34,14 @@ declare -A LAYOUTS=(
 # Primary monitor when using auto-layout for unknown configurations.
 declare PRIMARY=eDP-1
 
-# Helper function to extract the unique identifier (serial number or part of
-# EDID) of a monitor
+# Path to an icon to use in the desktop notification.
+declare NOTIFY_ICON=/usr/share/icons/candy-icons/preferences/scalable/preferences-desktop-display.svg
+
+# ----------------------------------------------------------------------------
+# Functions
+
+# Function to extract the unique identifier (serial number or part of EDID) of
+# a monitor.
 get_monitor_id() {
   local monitor_name="$1"
   local edid_full
@@ -42,6 +53,8 @@ get_monitor_id() {
   echo "${edid_hash}"
 }
 
+# Function to generate a unique identifier for the current monitor, comprised
+# of the unique identifiers of all connected monitors.
 generate_config_id() {
   local config_id=""
   local connected_monitors
@@ -52,16 +65,7 @@ generate_config_id() {
   echo "${config_id}" | md5sum | awk '{print $1}'
 }
 
-map_config_to_script() {
-  local config_id="$1"
-  while IFS='=' read -r hashed_edid friendly_name script_name; do
-    if [[ "${config_id}" == *"${hashed_edid}"* ]]; then
-      echo "${friendly_name}" "${script_name}"
-      return
-    fi
-  done < "${CONFIG_FILE}"
-}
-
+# ----------------------------------------------------------------------------
 # Main execution
 config_id=$(generate_config_id)
 layout_script=""
@@ -71,18 +75,28 @@ friendly_name=""
 for hash in "${!LAYOUTS[@]}"; do
   if [[ "${config_id}" == *"${hash}"* ]]; then
     IFS=':' read -r friendly_name layout_script <<< "${LAYOUTS[${hash}]}"
+    logger -t monitor-event.sh "Found matching entry for ${friendly_name} (${hash})"
     break
   fi
 done
 
-if [[ -n "${layout_script}" && -f "${layout_script}" ]]; then
+if [ -n "${layout_script}" ]; then
+  if [ ! -f "${layout_script}" ]; then
+    logger -t monitor-event.sh "Layout script not found: ${layout_script}"
+    exit 1
+  fi
+
+  logger -t monitor-event.sh "Executing layout for ${friendly_name} using script ${layout_script}"
   echo "Executing layout for ${friendly_name} using script ${layout_script}"
-  notify-send "Monitor layout changed to ${friendly_name}"
+  notify-send -i "$NOTIFY_ICON" "Monitor layout changed to ${friendly_name}"
   bash "${layout_script}"
   exit $?
 fi
 
+# ----------------------------------------------------------------------------
+# Fallback to auto-layout
 PREVIOUS=$PRIMARY
+logger -t monitor-event.sh "No layout script found for ${config_id}. Using auto-layout."
 for monitor in ${active_monitors}; do
   if [[ "${monitor}" != "${PRIMARY}" ]]; then
     echo "Executing xrandr --output ${monitor} --auto --right-of ${PREVIOUS}"
@@ -96,4 +110,4 @@ echo "Current configuration ID: ${config_id}"
 echo "Add the following entry to the LAYOUTS array in the script:"
 echo "[\"${config_id}\"]=\"ThisConfigName:path/to/your/layout_script.sh\""
 
-notify-send "Monitor layout not found for config ${config_id}"
+notify-send -i "$NOTIFY_ICON" "Monitor layout not found for config ${config_id}"
