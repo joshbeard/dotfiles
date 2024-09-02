d6353d1f7a0a66444781c5121168a89d8039e3b3
Josh Beard <josh@joshbeard.me>
2024-09-02 00:30:33 -0600

    tmux-sessionizer: quote paths and name

--- home/bin/tmux-sessionizer.sh
+++ home/bin/tmux-sessionizer.sh
@@ -32,6 +32,7 @@ get_projects() {
       "$projects"
       "$go_src"
       "$home"
+      "$HOME/Documents"
       "$configs"
       "$HOME/.homesick/repos/dotfiles"
     )
@@ -63,7 +64,7 @@ open() {
 }
 
 open_in_terminal() {
-  $TERMINAL -e tmux-sessionizer.sh $1
+  $TERMINAL -e tmux-sessionizer.sh "$1"
 }
 
 # Main logic
@@ -93,17 +94,17 @@ selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    open tmux new-session -s $selected_name -c $selected
+    open tmux new-session -s "$selected_name" -c "$selected"
     exit 0
 fi
 
-if ! tmux has-session -t $selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
+if ! tmux has-session -t "$selected_name" 2> /dev/null; then
+    tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
 if [ -z "$TMUX" ]; then
-    open tmux attach-session -t $selected_name
+    open tmux attach-session -t "$selected_name"
     exit 0
 fi
 
-open tmux switch-client -t $selected_name
+open tmux switch-client -t "$selected_name"
